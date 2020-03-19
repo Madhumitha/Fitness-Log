d816b50182498491ad0c1c3e272b15c0db8714a3
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2020-03-18 19:24:42 -0500

    Add models for authentication

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  User.associate = function({ AuthToken }) {
+    User.hasMany(AuthToken);
+  };
+
+  User.authenticate = async function(username, password) {
+    const user = await User.findOne({ where: { username } });
+
+    if (bcrypt.compareSync(password, user.password)) {
+      return user.authorize();
+    }
+
+    throw new Error("invalid password");
+  };
+
+  User.prototype.authorize = async function() {
+    const { AuthToken } = sequelize.models;
+    const user = this;
+
+    const authToken = await AuthToken.generate(this.id);
+
+    await user.addAuthToken(authToken);
+
+    return { user, authToken };
+  };
+
+  User.prototype.logout = async function(token) {
+    sequelize.models.AuthToken.destroy({ where: { token } });
+  };
+
+  return User;
+};
